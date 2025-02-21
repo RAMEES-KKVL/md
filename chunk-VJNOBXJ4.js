6ffb7b4f727881908a5ea23883301da68aa8d78e
RAMEES-KKVL <rameesp41750@gmail.com>
2025-02-21 13:58:57 +0530

    Initial commit of build files

---
+++ chunk-VJNOBXJ4.js
@@ -0,0 +1,1 @@
+import{Qa as n,cb as a,db as o,lb as s}from"./chunk-QKFPZ4VQ.js";var r=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-restaurant"]],decls:2,vars:0,template:function(e,i){e&1&&(a(0,"p"),s(1,"restaurant works!"),o())},encapsulation:2})};export{r as RestaurantComponent};
